8b86cc58513fb77dba2e17bde25175ca6cdd8737
dgoorskey <dersu.goorskey@gmail.com>
2023-08-10 00:20:08 -0400

    improve tests

--- tests/test_vm_op_add.c
+++ tests/test_vm_op_add.c
@@ -3,7 +3,7 @@
 #include "../vm.h"
 
 void
-test (word_t a, word_t b, word_t expected_sum)
+test (word_t a, word_t b, word_t expected_result, int expected_status)
 {
     struct VM vm;
     vm_reset (&vm);
@@ -11,29 +11,19 @@ test (word_t a, word_t b, word_t expected_sum)
     vm.register_a = a;
     vm.register_b = b;
 
-    /* if overflow should occur */
-    if (expected_sum < a || expected_sum < b)
-    {
-        assert (vm_op_add (&vm) == VM_STATUS_OVERFLOW);
-        assert (vm.status == VM_STATUS_OVERFLOW);
-    }
-    else
-    {
-        assert (vm_op_add (&vm) == VM_STATUS_OK);
-        assert (vm.status == VM_STATUS_OK);
-    }
-
-    assert (vm.register_a == expected_sum);
+    assert (vm_op_add (&vm) == expected_status);
+    assert (vm.status == expected_status);
+    assert (vm.register_a == expected_result);
 }
 
 int
 main (void)
 {
-    test (0, 0, 0);
-    test (1, 1, 2);
-    test (WORD_T_MAX, 0, WORD_T_MAX);
-    test (WORD_T_MAX, 1, 0);
-    test (WORD_T_MAX, WORD_T_MAX, WORD_T_MAX - 1);
+    test (0, 0, 0, VM_STATUS_OK);
+    test (1, 1, 2, VM_STATUS_OK);
+    test (WORD_T_MAX, 0, WORD_T_MAX, VM_STATUS_OK);
+    test (WORD_T_MAX, 1, 0, VM_STATUS_OVERFLOW);
+    test (WORD_T_MAX, WORD_T_MAX, WORD_T_MAX - 1, VM_STATUS_OVERFLOW);
 
     return 0;
 }
